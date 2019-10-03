amboar/skiboot

commit 29d1e6f7810982d2c8f057ae35a39c2ac0cf484b

libflash/blocklevel: add a smart write function which wraps up eraseing and writing
For consumers who can't do better or who aren't to phased about
performance, having to book keep erasing in order to be able to write data
can be painful. Blocklevel can do it, possibly naively but its a convenience
function

Reviewed-By: Alistair Popple <alistair@popple.id.au>
Signed-off-by: Cyril Bur <cyril.bur@au1.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

libflash/blocklevel.c
@@ -158,6 +158,77 @@ int blocklevel_get_info(struct blocklevel_device *bl, const char **name, uint32_
 	return rc;
 }
 
+int blocklevel_smart_write(struct blocklevel_device *bl, uint32_t pos, const void *buf, uint32_t len)
+{
+	uint32_t erase_size;
+	const void *write_buf = buf;
+	void *write_buf_start = NULL;
+	void *erase_buf;
+	int rc = 0;
+
+	if (!write_buf || !bl) {
+		errno = EINVAL;
+		return FLASH_ERR_PARM_ERROR;
+	}
+
+	if (!(bl->flags & WRITE_NEED_ERASE))
+		return blocklevel_write(bl, pos, buf, len);
+
+	rc = blocklevel_get_info(bl, NULL, NULL, &erase_size);
+	if (rc)
+		return rc;
+
+	if (ecc_protected(bl, pos, len)) {
+		len = ecc_buffer_size(len);
+
+		write_buf_start = malloc(len);
+		if (!write_buf_start) {
+			errno = ENOMEM;
+			return FLASH_ERR_MALLOC_FAILED;
+		}
+
+		if (memcpy_to_ecc(write_buf_start, buf, ecc_buffer_size_minus_ecc(len))) {
+			free(write_buf_start);
+			errno = EBADF;
+			return FLASH_ERR_ECC_INVALID;
+		}
+		write_buf = write_buf_start;
+	}
+
+	erase_buf = malloc(erase_size);
+	if (!erase_buf) {
+		errno = ENOMEM;
+		rc = FLASH_ERR_MALLOC_FAILED;
+		goto out;
+	}
+
+	while (len > 0) {
+		uint32_t erase_block = pos & ~(erase_size - 1);
+		uint32_t block_offset = pos & (erase_size - 1);
+		uint32_t size = erase_size > len ? len : erase_size;
+
+		rc = bl->read(bl, erase_block, erase_buf, erase_size);
+		if (rc)
+			goto out;
+
+		if (memcmp(erase_buf + block_offset, write_buf, size) != 0) {
+			memcpy(erase_buf + block_offset, write_buf, size);
+			rc = bl->write(bl, erase_block, erase_buf + block_offset, size);
+			if (rc)
+				goto out;
+		}
+
+		len -= size;
+		pos += size;
+		write_buf += size;
+	}
+
+out:
+	free(write_buf_start);
+	free(erase_buf);
+	return rc;
+}
+
 static int insert_bl_prot_range(struct blocklevel_range *ranges, struct bl_prot_range range)
 {
 	struct bl_prot_range *new_ranges;

libflash/blocklevel.h
@@ -60,6 +60,9 @@ int blocklevel_erase(struct blocklevel_device *bl, uint32_t pos, uint32_t len);
 int blocklevel_get_info(struct blocklevel_device *bl, const char **name, uint32_t *total_size,
 		uint32_t *erase_granule);
 
+/* Convienience functions */
+int blocklevel_smart_write(struct blocklevel_device *bl, uint32_t pos, const void *buf, uint32_t len);
+
 /* Implemented in software at this level */
 int blocklevel_ecc_protect(struct blocklevel_device *bl, uint32_t start, uint32_t len);
 

commit 8c88f4a369c36ae633d03680561dcea22b436c08

blocklevel: smart_write: Unaligned writes to ECC partitions
Currently trying to clear a gard record results in errors:

    $ ./opal-gard -pef part create /sys0/node0/proc1
    $ ./opal-gard -pef part list
     ID       | Error    | Type       | Path
    ---------------------------------------------------------
     00000001 | 00000000 | Manual     | /Sys0/Node0/Proc1
    =========================================================
    $ ./opal-gard -pef part clear 00000001
    Clearing gard record 0x00000001...done
    ECC: uncorrectable error: ffffff00ffffffff ff
    libflash ecc invalid
    $

blocklevel_smart_write() turned out to not be quite as smart as it thought it
was in that any unaligned write to ECC protected partitions resulted in
corruption.

Fixes: 29d1e6f78109 ("libflash/blocklevel: add a smart write function which wraps up eraseing and writing")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

libflash/blocklevel.c
@@ -513,7 +513,7 @@ int blocklevel_smart_write(struct blocklevel_device *bl, uint64_t pos, const voi
 	void *erase_buf = NULL;
 	uint32_t erase_size;
 
-	const void *write_buf = buf;
+	const void *write_buf;
 	uint64_t write_len;
 	uint64_t write_pos;
 
@@ -543,26 +543,45 @@ int blocklevel_smart_write(struct blocklevel_device *bl, uint64_t pos, const voi
 	}
 
 	if (ecc_protection) {
+		uint64_t ecc_pos, ecc_align, ecc_diff, ecc_len;
+
 		FL_DBG("%s: region has ECC\n", __func__);
 
-		len = ecc_buffer_size(len);
+		ecc_pos = with_ecc_pos(ecc_start, pos);
+		ecc_align = ecc_buffer_align(ecc_start, ecc_pos);
+		ecc_diff = ecc_pos - ecc_align;
+		ecc_len = ecc_buffer_size(len + ecc_diff);
 
-		ecc_buf = malloc(len);
+		ecc_buf = malloc(ecc_len);
 		if (!ecc_buf) {
 			errno = ENOMEM;
 			return FLASH_ERR_MALLOC_FAILED;
 		}
 
-		if (memcpy_to_ecc(ecc_buf, buf, ecc_buffer_size_minus_ecc(len))) {
+		if (ecc_diff) {
+			rc = blocklevel_read(bl, ecc_align, ecc_buf, ecc_diff);
+			if (rc) {
+				errno = EBADF;
+				rc = FLASH_ERR_ECC_INVALID;
+				goto out;
+			}
+		}
+
+		rc = memcpy_to_ecc_unaligned(ecc_buf, buf, len, ecc_diff);
+		if (rc) {
 			free(ecc_buf);
 			errno = EBADF;
 			return FLASH_ERR_ECC_INVALID;
 		}
-		write_buf = ecc_buf;
-	}
 
-	write_pos = pos;
-	write_len = len;
+		write_buf = ecc_buf;
+		write_len = ecc_len;
+		write_pos = ecc_pos;
+	} else {
+		write_buf = buf;
+		write_len = len;
+		write_pos = pos;
+	}
 
 	erase_buf = malloc(erase_size);
 	if (!erase_buf) {

external/gard/test/results/11-clear-first.out
@@ -1,0 +1,13 @@
+Clearing the entire gard partition...done
+ ID       | Error    | Type       | Path
+---------------------------------------------------------
+ 00000001 | 00000000 | Manual     | /Sys0/Node0/Proc0
+ 00000002 | 00000000 | Manual     | /Sys0/Node0/Proc1
+=========================================================
+Clearing gard record 0x00000001...done
+ ID       | Error    | Type       | Path
+---------------------------------------------------------
+ 00000002 | 00000000 | Manual     | /Sys0/Node0/Proc1
+=========================================================
+Clearing gard record 0x00000002...done
+No GARD entries to display

external/gard/test/tests/11-clear-first
@@ -1,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+DATA=$(mktemp)
+
+cleanup() {
+	rm -f $DATA
+}
+
+trap cleanup EXIT
+
+dd if=/dev/zero of=$DATA bs=$((0x1000)) count=5 2>/dev/null
+
+run_binary "./opal-gard" "-p -e -f $DATA clear all"
+run_binary "./opal-gard" "-p -e -f $DATA create /sys0/node0/proc0"
+run_binary "./opal-gard" "-p -e -f $DATA create /sys0/node0/proc1"
+run_binary "./opal-gard" "-p -e -f $DATA list"
+run_binary "./opal-gard" "-p -e -f $DATA clear 00000001"
+run_binary "./opal-gard" "-p -e -f $DATA list"
+run_binary "./opal-gard" "-p -e -f $DATA clear 00000002"
+run_binary "./opal-gard" "-p -e -f $DATA list"
+
+diff_with_result
+
+pass_test